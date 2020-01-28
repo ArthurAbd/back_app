5f22a58bb34636163b9fa41c5c6584df9ef9c499
Artur Abdrakhmanov <aabdrakhmanov@stack-kazan.net>
2020-01-29 00:58:22 +0300

    fixed /user/login

--- db/user/index.js
+++ db/user/index.js
@@ -14,14 +14,30 @@ async function findUserById(id) {
         });
 }
 
-async function findUserByNumber(number) {
+async function findUserByidPhoneNumber(id) {
     return knex('user')
-        .where('number', number)
+        .where('idPhoneNumber', id)
+        .select('userId')
         .then(res => {
-            if (!res) undefined
+            if (!res[0]) {return undefined}
             return res[0]
-        });
+        })
 }
+
+async function findNumberOrCreate(number) {
+    return knex('phone_number')
+        .where('number', number)
+        .select('idPhoneNumber')
+        .then(res => {
+            if (res[0]) {return res[0].idPhoneNumber}
+            return knex('phone_number')
+                .insert({number})
+                .then(res => res[0])
+        })
+}
+
+
+// findUserByidPhoneNumber(2).then((res) => console.log(!!res))
 
 async function editUser(data, id) {
     return knex('user')
@@ -34,5 +50,6 @@ module.exports = {
     editUser,
     addUser,
     findUserById,
-    findUserByNumber
+    findUserByidPhoneNumber,
+    findNumberOrCreate
 }
--- routes/user.js
+++ routes/user.js
@@ -4,25 +4,50 @@ const dbUser = require('../db/user');
 const dbOauth = require('../db/oauth');
 const passport = require('passport');
 const authHelper = require('../helpers/auth');
-const oauth2 = require('../helpers/oauth2')
+const oauth2 = require('../helpers/oauth2');
+
+
+router.post('/add', async (req, res) => {
+  try {
+    const {name, number, password} = req.body;
+
+    const idPhoneNumber = await dbUser.findNumberOrCreate(number);
+    const user = await dbUser.findUserByidPhoneNumber(idPhoneNumber);
+    if (user) {return res.status(403).json('Пользователь уже существует')}
+
+    const dataUser = {
+        name,
+        idPhoneNumber,
+        password: authHelper.encryptPassword(password, idPhoneNumber.toString()),
+        created: Date.now()
+    }
+
+    const newUserId = await dbUser.addUser(dataUser);
+    if (newUserId[0]) {return res.status(200).json('Пользователь добавлен')}
+
+  } catch (error) {
+      res.status(500)
+      .json('Внутренняя ошибка сервера');
+  }
+});
+
 
 router.post('/login', async (req, res, next) => {
-  console.log(req.body)
-      try {
-          const {username, password} = req.body;
-          const user = await dbUser.findUserByNumber(username)
+  try {
+      const {username, password} = req.body;
+      const user = await dbUser.findUserByNumber(username)
 
-          if (!user) {return res.status(403).json('Пользователь не найден')}
-          if (authHelper.checkPassword(password, user.password, user.number)) {
-            return next()
-          }
-          res.status(403).json('Не верный пароль')
-      } catch (error) {
-        console.log('error', error)
-          res.status(500)
-          .json('Ошибка на сервере');
+      if (!user) {return res.status(403).json('Пользователь не найден')}
+      if (authHelper.checkPassword(password, user.password, user.number)) {
+        return next()
       }
-  }, oauth2.token)
+      res.status(403).json('Не верный пароль')
+  } catch (error) {
+    console.log('error', error)
+      res.status(500)
+      .json('Ошибка на сервере');
+  }
+}, oauth2.token)
 
 router.post('/me',
   passport.authenticate('bearer', { session: false }),
@@ -48,30 +73,6 @@ router.post('/edit',
           .json('Error');
       }
   });
-
-router.post('/add', async (req, res) => {
-  console.log(req.body)
-    try {
-        const data = req.body;
-        const info = {
-            name: data.name,
-            number: data.number,
-            password: authHelper.encryptPassword(data.password, data.number),
-            created: Date.now()
-        }
-        const isUser = await dbUser.findUserByNumber(data.number);
-        if (isUser) {return res.status(403).json('Пользователь уже существует')}
-        
-        const id = await dbUser.addUser(info);
-        if (id) {return res.status(200).json('Пользователь добавлен')}
-
-        res.status(200)
-        .json('Неизвестная ошибка');
-    } catch (error) {
-        res.status(500)
-        .json('Внутренняя ошибка сервера');
-    }
-});
 
 router.post('/profile',
   passport.authenticate('bearer', { session: false }),
